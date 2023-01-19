505d99d9c042391eddfd904862f475c2eab69e1d
tushar <tusharjadhav9997@gmail.com>
2023-01-19 10:06:35 +0530

    new file created

---
+++ src/com/velocity/com/TreeSetDemo.java
@@ -0,0 +1,20 @@
+package com.velocity.com;
+
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+
+	public static void main(String[] args) {
+		
+		TreeSet hs = new TreeSet();
+		
+		hs.add(145);
+		hs.add(789);
+		hs.add(456);
+		hs.add(789);
+		System.out.println(hs);
+		
+
+	}
+
+}
