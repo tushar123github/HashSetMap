306d369975e026070d2ca0754ec47e65e517aeda
tushar <tusharjadhav9997@gmail.com>
2023-01-19 09:55:19 +0530

    Added the sucesss

--- src/com/velocity/com/HashDemo.java
+++ src/com/velocity/com/HashDemo.java
@@ -1,10 +1,17 @@
 package com.velocity.com;
 
+import java.util.ArrayList;
+
 public class HashDemo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Velocity corporate training center");
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		al.add(120);
+		al.add(150);
+		al.add(170);
+		al.add(190);
+		System.out.println(al);
 
 	}
 
---
+++ src/com/velocity/com/LinkedListDemo.java
@@ -0,0 +1,17 @@
+package com.velocity.com;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		
+		LinkedList<Integer> list = new LinkedList <Integer>();
+		list.add(145);
+		list.add(170);
+		list.add(200);
+		System.out.println(list);
+
+	}
+
+}
